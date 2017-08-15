81ea69c95baf21144955287af18d44cdc7b3e90c
Barbara Śmiech <b.smiech@wp.pl>
2017-08-16 00:41:05 +0200

    longer version

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,10 @@ var a = 3;
 var b = 4;
 var value = (a * a) + (2 * a * b) - (b * b);
 console.log(value);
-var positiveOrNegative = value > 0 ? 'variable value is positive' : 'variable value is negative';
-console.log(positiveOrNegative);
-var equalsNull = value = 0 ? "variable value equals 0" : "variable value doesn't equal 0";
-console.log(equalsNull);
+if (value > 0) {
+    console.log('variable value is positive');
+} else if (value < 0) {
+    console.log('variable value is negative');
+} else {
+    console.log('variable value equals 0');
+}

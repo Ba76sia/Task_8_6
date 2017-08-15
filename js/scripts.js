93bf39584f2ef4e68315de2b0a40fa15ec929e23
Barbara Śmiech <b.smiech@wp.pl>
2017-08-15 15:20:15 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var a = 3;
+var b = 4;
+var value = (a * a) + (2 * a * b) - (b * b);
+console.log(value);
+var positiveOrNegative = value > 0 ? 'variable value is positive' : 'variable value is negative';
+console.log(positiveOrNegative);
+var equalsNull = value = 0 ? "variable value equals 0" : "variable value doesn't equal 0";
+console.log(equalsNull);
